027b78d5bf4d5c1db7ff68f73064b39204478e25
Stephen <sheldon2521@gmail.com>
2019-04-05 18:27:19 -0400

    Added a directed DFS option. Gives user the option the see if you can reach one vertex from a given source vertex.

--- src/DigraphTest.java
+++ src/DigraphTest.java
@@ -1,4 +1,5 @@
 import com.ssheld.Graph.Digraph;
+import com.ssheld.Search.DirectedDFS;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -18,7 +19,7 @@ public class DigraphTest {
         Scanner fin;
         Scanner scan = new Scanner(System.in);
         String fileName;
-        int vCount, eCount, v, w;
+        int vCount, eCount, v, w, source, target;
         Digraph G = new Digraph(1);
 
         System.out.println("Please specify the name of the file you wish to generate a graph from");
@@ -45,10 +46,13 @@ public class DigraphTest {
         System.out.println("Please enter your choice.");
         do {
             System.out.println("1. Print the graph as scanner in");
+            System.out.println("2. Reverse the Digraph and print the result");
+            System.out.println("3. Check if there is a directed path from source s to a given target vertex v");
             System.out.println("0. Quit the program.");
 
             choice = scan.nextInt();
 
+            System.out.println();
 
             if (choice == 1) {
                 for (int i = 0; i < G.V(); i++) {
@@ -59,6 +63,32 @@ public class DigraphTest {
                     System.out.println("");
                 }
             }
+            else if (choice == 2) {
+                Digraph reversedGraph = G.reverse();
+                for (int i = 0; i < reversedGraph.V(); i++) {
+                    System.out.printf("%d :", i);
+                    for (int j : reversedGraph.adj(i)) {
+                        System.out.printf(" %d ", j);
+                    }
+                    System.out.println("");
+                }
+            }
+            else if (choice == 3) {
+                System.out.println("Please enter the source vertex");
+                source = scan.nextInt();
+                System.out.println("Please enter the target vertex");
+                target = scan.nextInt();
+                DirectedDFS dDFS = new DirectedDFS(G, source);
+
+                if (dDFS.marked(target)) {
+                    System.out.printf("Yes you can reach %d from %d%n", target, source);
+                }
+                else {
+                    System.out.printf("No you can not reach %d from %d%n", target, source);
+                }
+            }
+
+            System.out.println();
         } while (choice != 0);
     }
 }
---
+++ src/com/ssheld/Search/DirectedDFS.java
@@ -0,0 +1,44 @@
+package com.ssheld.Search;
+
+import com.ssheld.Graph.Digraph;
+
+/**
+ * Author: Stephen Sheldon 4/5/2019
+ *
+ * Depth first search for a directed graph
+ *
+ */
+
+public class DirectedDFS {
+    private boolean marked[];
+
+    // Find vertices in G that are reachable from S
+    public DirectedDFS(Digraph G, int s) {
+        marked = new boolean[G.V()];
+        dfs(G,s);
+    }
+
+    // Find vertices in G that are reachable from sources
+    public DirectedDFS(Digraph G, Iterable<Integer> sources) {
+        marked = new boolean[G.V()];
+        for (int s : sources) {
+            if (!marked[s]) {
+                dfs(G, s);
+            }
+        }
+    }
+
+    // Check if V is reachable
+    public boolean marked(int v) {
+        return marked[v];
+    }
+
+    private void dfs(Digraph G, int v) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G, w);
+            }
+        }
+    }
+}
