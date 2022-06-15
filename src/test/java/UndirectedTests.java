c8b3a9400af25e56d7772d2d74e5326cf2320262
ssheld <20730278+ssheld@users.noreply.github.com>
2022-06-15 11:04:56 -0400

    create dfs test case for connected graph

--- src/test/java/UndirectedTests.java
+++ src/test/java/UndirectedTests.java
@@ -1,4 +1,6 @@
+import com.ssheld.graphs.Graph;
 import com.ssheld.graphs.ListGraph;
+import com.ssheld.search.DepthFirstSearch;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -6,9 +8,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UndirectedTests {
 
-    ListGraph myListGraph;
+    ListGraph myUnconnectedListGraph;
+    ListGraph myConnectedListGraph;
 
     // Source Vertex
     int source;
@@ -23,20 +28,39 @@ public class UndirectedTests {
 
     @BeforeEach
     void setup() {
-        myListGraph = new ListGraph(0);
+        myUnconnectedListGraph = new ListGraph(0);
         Scanner scan = new Scanner(System.in);
         Scanner fin;
-        fileName = "smallGraph.txt";
+        fileName = "unconnectedSmallGraph.txt";
         try {
             fin = new Scanner(new File(fileName));
             vCount = Integer.parseInt(fin.nextLine());
             eCount = Integer.parseInt(fin.nextLine());
             // User has chosen to use an adjacency list
-            myListGraph = new ListGraph(vCount);
+            myUnconnectedListGraph = new ListGraph(vCount);
             for (int i = 0; i < eCount; i++) {
                 v = fin.nextInt();
                 w = fin.nextInt();
-                myListGraph.addEdge(v, w);
+                myUnconnectedListGraph.addEdge(v, w);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Sorry, the file you specified could not be found.");
+        }
+
+        myConnectedListGraph = new ListGraph(0);
+        scan = new Scanner(System.in);
+        fileName = "connectedSmallGraph.txt";
+        try {
+            fin = new Scanner(new File(fileName));
+            vCount = Integer.parseInt(fin.nextLine());
+            eCount = Integer.parseInt(fin.nextLine());
+            // User has chosen to use an adjacency list
+            myConnectedListGraph = new ListGraph(vCount);
+            for (int i = 0; i < eCount; i++) {
+                v = fin.nextInt();
+                w = fin.nextInt();
+                myConnectedListGraph.addEdge(v, w);
             }
 
         } catch (FileNotFoundException e) {
@@ -45,8 +69,40 @@ public class UndirectedTests {
     }
 
     @Test
-    void printListGraph() {
-        System.out.println("You have the following graph:");
-        myListGraph.printGraph();
+    void depthFirstSearchUnconnectedGraph() {
+        System.out.println("You have the following unconnected graph:");
+        myUnconnectedListGraph.printGraph();
+
+        System.out.println("Finding all connected vertices to 0");
+        DepthFirstSearch dfs = new DepthFirstSearch(myUnconnectedListGraph, 0);
+        for (int v = 0; v < myUnconnectedListGraph.getNumVertices(); v++) {
+            if (dfs.marked(v)) {
+                System.out.printf("%d ", v);
+            }
+        }
+
+        System.out.printf("\n");
+
+        // Check if graph is connected
+        assertEquals(false, dfs.count() == myUnconnectedListGraph.getNumVertices());
+    }
+
+    @Test
+    void depthFirstSearchConnectedGraph() {
+        System.out.println("You have the following connected graph:");
+        myConnectedListGraph.printGraph();
+
+        System.out.println("Finding all connected vertices to 0");
+        DepthFirstSearch dfs = new DepthFirstSearch(myConnectedListGraph, 0);
+        for (int v = 0; v < myConnectedListGraph.getNumVertices(); v++) {
+            if (dfs.marked(v)) {
+                System.out.printf("%d ", v);
+            }
+        }
+
+        System.out.printf("\n");
+
+        // Check if graph is connected
+        assertEquals(true, dfs.count() == myConnectedListGraph.getNumVertices());
     }
 }
